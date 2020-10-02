edfe8efe6f343425be8b27142b88edb66a3081ad
ardy <ardygh@berkeley.edu>
2020-10-02 11:34:01 -0700

    snake project skeleton for my python kids

---
+++ snakePython/snakeSkeletonCode.py
@@ -0,0 +1,134 @@
+"""
+Snake with PyGame 
+Ardy Ghoorchian 
+"""
+
+from gui import*
+
+# Difficulty settings
+# Easy      ->  10
+# Medium    ->  25
+# Hard      ->  40
+# Harder    ->  60
+# Impossible->  120
+difficulty = 25
+
+
+# Game variables
+snake_pos = [100, 50]
+snake_body = [[100, 50], [100-10, 50], [100-(2*10), 50]]
+
+food_pos = [random.randrange(1, (frame_size_x//10)) * 10, random.randrange(1, (frame_size_y//10)) * 10]
+food_spawn = True
+
+direction = 'RIGHT'
+change_to = direction
+
+
+
+# Main logic
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            # W -> Up; S -> Down; A -> Left; D -> Right
+            if event.key == pygame.K_UP or event.key == ord('w'):
+                change_to = 'UP'
+            if event.key == pygame.K_DOWN or event.key == ord('s'):
+                change_to = 'DOWN'
+            if event.key == pygame.K_LEFT or event.key == ord('a'):
+                change_to = 'LEFT'
+            if event.key == pygame.K_RIGHT or event.key == ord('d'):
+                change_to = 'RIGHT'
+            # Esc -> Create event to quit the game
+            if event.key == pygame.K_ESCAPE:
+                pygame.event.post(pygame.event.Event(pygame.QUIT))
+
+    
+    """
+    The line above listens for an event. which in this case is when you press the key while you are playing the game and based on
+    What you press, it will change the "change_to" variable. it is not so important for you to understand how that happens
+    what is really important for you to understand is that after that infromation is given to you now how do you change the 
+    poisition of your snake.
+
+    1.  Making sure the snake cannot move in the opposite direction instantaneously
+    """
+    # We will write te psudo code for it first  
+    # ------
+    # ------
+    # ------
+    # FOR YOU TO DO 
+
+
+
+    """
+    2. Moving the snake
+    """
+
+    # ------
+    # ------
+    # ------
+    # FOR YOU TO DO
+   
+   """
+   3.Snake body growing mechanism
+   replace the commented line with code
+   """
+    snake_body.insert(0, list(snake_pos))
+    if snake_pos[0] == food_pos[0] and snake_pos[1] == food_pos[1]:
+        # FOR YOU TO DO
+        # update the score
+        # set "food_spawn" to False
+    else:
+        snake_body.pop()
+
+    
+    """
+    4.Spawning food on the screen
+    you do not need to writ anything for this part but you should be able to understand what it means 
+    """
+    if not food_spawn:
+        food_pos = [random.randrange(1, (frame_size_x//10)) * 10, random.randrange(1, (frame_size_y//10)) * 10]
+        # You should be able explain to me what the code above means 
+    food_spawn = True
+
+
+    # The code below is in charge of GUI code (NOT FOR YOU)
+    # ASK ME IF YOU ARE CURIOUS TO LEARN ABOUT THESE LINES 
+    game_window.fill(black)
+    for pos in snake_body:
+        pygame.draw.rect(game_window, green, pygame.Rect(pos[0], pos[1], 10, 10))
+    pygame.draw.rect(game_window, white, pygame.Rect(food_pos[0], food_pos[1], 10, 10))
+
+
+
+
+    """
+    4.Game Over conditions
+    uncomment the code if it is commented and fill in the blanks 
+    The code is in charge of  calling the game over function when we break the rules of the game
+    a.  when we go out of bounds 
+    b.  when we hit ourseleves 
+    """
+
+    
+    
+    # if snake_pos[0] < -------- or snake_pos[0] > ---------:
+    #     game_over()
+    # if snake_pos[1] < -------- or snake_pos[1] > ---------:
+    #     game_over()
+    # # Touching the snake body
+    # for block in snake_body[1:]:
+    #     if snake_pos[0] == block[0] and snake_pos[1] == block[1]:
+    #         game_over()
+
+
+
+
+
+    # DO NOT WORRY ABOUT THE CODE BELOW 
+    show_score(1, white, 'consolas', 20)
+    pygame.display.update()
+    fps_controller.tick(difficulty)
